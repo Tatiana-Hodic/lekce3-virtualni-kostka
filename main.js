eabbd50ee82306788ef60a91c592c7695efe1c8a
Tatiana Hodic <tatiana.hodic.miklos@gmail.com>
2021-03-24 15:47:27 +0100

    cube done

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // tady je místo pro náš program
+
+function cislo() {
+    let cislo = Math.ceil(Math.random() * 6);
+    document.querySelector('#kostka').src = 'obrazky/' + cislo + '.png';
+
+    if (cislo === 6) {
+        document.querySelector('#zprava').innerHTML = 'Vyhrála si. Hodila si číslo ' + cislo;
+    } else {
+        document.querySelector('#zprava').innerHTML = 'Hodila si číslo ' + cislo;
+    } 
+}
+
+
